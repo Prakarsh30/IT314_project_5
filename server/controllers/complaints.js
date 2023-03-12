733a2bbcbce1e3100f7d36b57489b64ba7d7394f
Kirtan Shah <202001043@daiict.ac.in>
2023-03-12 20:12:13 +0530

    first commit

--- server/controllers/complaints.js
+++ server/controllers/complaints.js
@@ -1,5 +1,24 @@
-const getComplaints = (req, res) => {
-  res.send("complaints");
+const complaintMessage = require("../models/complaints");
+
+const getComplaints = async (req, res) => {
+  try {
+    const complaintMessages = await complaintMessage.find();
+    res.status(200).json(complaintMessages);
+  } catch (error) {
+    res.status(409).send({ message: error.message });
+  }
 };
 
-module.exports = { getComplaints };
+const createComplaints = async (req, res) => {
+  const complaint = req.body;
+
+  const newComplaint = new complaintMessage(complaint);
+  try {
+    await newComplaint.save();
+    res.status(200).send(newComplaint);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
+
+module.exports = { getComplaints, createComplaints };
