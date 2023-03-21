40cb82afedab7f7c6a4c13d31cfeaef4196be735
hcp4902 <hcp4902@gmail.com>
2023-03-22 01:05:19 +0530

    Login page

--- client/src/components/Login_page/login.js
+++ client/src/components/Login_page/login.js
@@ -1,20 +1,64 @@
-import React from "react";
+
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 import "./login_styles.css";
 
+export default function LoginPage() {
 
-function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
   return (
-    <div>
-    <div className="mb-3">
-      <label for="exampleFormControlInput1" className="form-label">Email address</label>
-      <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+    <div className="login-container">
+      <h3 className="title">Welcome Back User</h3>
+      <Form onSubmit={handleSubmit}>
+        <div className="fluid-input">
+        <Form.Group size="lg" controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            autoFocus
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="fluid-input-input"
+          />
+        </Form.Group>
+        </div>
+
+        <div className="fluid-input">
+        <Form.Group size="lg" controlId="password"> 
+
+          <Form.Label>Password:</Form.Label>
+
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="fluid-input-input"
+          />
+
+        </Form.Group>
+        </div>
+
+        <div>
+        <Button block size="lg" type="submit" disabled={!validateForm()} className="login-button">
+          Login
+        </Button>
+        </div>
+      </Form>
+
     </div>
-    <div className="mb-3">
-      <label for="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-      <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-    </div>
-    </div>
+
   );
+
 }
 
-export default LoginPage;
+
