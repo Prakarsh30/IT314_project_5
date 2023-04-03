d5d0d38fa51ce6813ff01aec7e409a194fd98cdf
vishrutshah21 <85744281+vishrutshah21@users.noreply.github.com>
2023-04-03 19:07:02 +0530

    Update courier.js

--- server/models/courier.js
+++ server/models/courier.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const courier = mongoose.Schema({
   student_name: String,
   ID: Number,
+  description: String
   creator: String,
   createdAt: {
     type: Date,
