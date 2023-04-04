bf7cd6eb88871f08454b21391225dd4b8d553446
romittejani <83640805+romittejani@users.noreply.github.com>
2023-04-04 22:12:39 +0530

    Update notice.js

--- server/controllers/notice.js
+++ server/controllers/notice.js
@@ -1,5 +1,37 @@
-const getNotice = (req, res) => {
-  res.send("notice");
+const noticeMessage = require("../models/notices");
+
+const getNotices = async (req, res) => {
+  try {
+    const noticeMessages = await noticeMessage.find();
+    res.status(200).json(noticeMessages);
+  } catch (error) {
+    res.status(409).send({ message: error.message });
+  }
 };
 
-module.exports = { getNotice };
+const createNotices = async (req, res) => {
+  const notice = req.body;
+
+  const newNotice = new noticeMessage(notice);
+  try {
+    await newNotice.save();
+    res.status(200).send(newNotice);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
+
+exports.deleteNotice = async (req, res) => {
+  // delete requested ID
+  const { _id } = req.body;
+
+  const notice = await noticeMessage.findByIdAndDelete(_id);
+
+  try {
+    res.status(201).json({ message: "Successful" });
+  } catch (err) {
+    res.status(409).json({ message: err.message });
+  }
+};
+
+module.exports = { getNotices, createNotices};
