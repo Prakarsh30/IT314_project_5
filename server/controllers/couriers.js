aad143336a7bd70bac95869cab3a539563f632bf
romittejani <83640805+romittejani@users.noreply.github.com>
2023-04-04 21:05:59 +0530

    Update couriers.js

--- server/controllers/couriers.js
+++ server/controllers/couriers.js
@@ -1,5 +1,41 @@
-const getCouriers = (req, res) => {
-  res.send("couriers");
-};
+const courierMessage = require("../models/couriers");
 
-module.exports = { getCouriers };
+const getCouriers = async (req, res) => {
+    try {
+        const courierMessages = await courierMessage.find();
+        res.status(200).json(courierMessages);
+      } catch (error) {
+        res.status(409).send({ message: error.message });
+      }
+  };
+
+const createCouriers = async (req, res) => {
+    const courier = req.body;
+  
+    const newCourier = new courierMessage(courier);
+    try {
+      await newCourier.save();
+      res.status(200).send(newCourier);
+    } catch (error) {
+      res.status(409).json({ message: error.message });
+    }
+};
+  
+exports.deleteCouriers = async(req, res) =>{
+    // delete requested ID
+    const {_id} = req.body;
+
+    const notice = await noticeMessage.findByIdAndDelete(_id);
+  
+    // console.log(notice);
+    try{
+        res.status(201).json({message: "Successful"});
+    }catch(err){
+      // console.log("not deleted");
+        res.status(409).json({message: err.message});
+    }
+    
+}
+
+
+  module.exports = { getCouriers, createCouriers, deleteCouriers };
