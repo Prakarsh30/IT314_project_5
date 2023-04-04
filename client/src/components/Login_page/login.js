fee7d88518e80cd71c0902574cf82fbf587b46a7
Prakarsh-Mathur02 <202001012@daiict.ac.in>
2023-04-04 23:29:15 +0530

    Some changes in NoticeBoard(1)

--- client/src/components/Login_page/login.js
+++ client/src/components/Login_page/login.js
@@ -68,7 +68,7 @@ export default function LoginPage() {
           <Form onSubmit={handleSubmit}>
             <form className="login">
               <div className="login__field">
-                <FaUserAlt i className="login__icon fas fa-user" />
+                <FaUserAlt i className="login__icon" />
 
                 <Form.Group size="lg" controlId="email">
                   <Form.Control
@@ -77,13 +77,13 @@ export default function LoginPage() {
                     onChange={(e) => setEmail(e.target.value)}
                     type="text"
                     className="login__input"
-                    placeholder="User name / Email"
+                    placeholder="Username/Email"
                   />
                 </Form.Group>
               </div>
 
               <div className="login__field">
-                <FaLock i className="login__icon fas fa-lock" />
+                <FaLock i className="login__icon" />
                 <Form.Group size="lg" controlId="password">
                   <Form.Control
                     value={password}
