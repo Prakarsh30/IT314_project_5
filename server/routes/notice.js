f6c87c6ad8caaa4827e2bf89304911700431cbf8
Kamal Patel <kamal1402patel@gmail.com>
2023-04-04 18:42:15 +0530

    Notice page get post request done.

--- server/routes/notice.js
+++ server/routes/notice.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { getNotice } = require("../controllers/notice");
+const { getNotice, postNotice } = require("../controllers/notice");
 router.get("/", getNotice);
+router.post("/", postNotice);
+
 
 module.exports = router;
+
