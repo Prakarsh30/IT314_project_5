d1e6ae1e8990b28e7350e3f33a049f0abf11558e
Kirtan Shah <202001043@daiict.ac.in>
2023-04-06 22:55:04 +0530

    Added environment files

--- server/controllers/couriers.js
+++ server/controllers/couriers.js
@@ -1,41 +1,39 @@
-const courierMessage = require("../models/couriers");
+const courierMessage = require("../models/courier");
 
 const getCouriers = async (req, res) => {
-    try {
-        const courierMessages = await courierMessage.find();
-        res.status(200).json(courierMessages);
-      } catch (error) {
-        res.status(409).send({ message: error.message });
-      }
-  };
+  try {
+    const courierMessages = await courierMessage.find();
+    res.status(200).json(courierMessages);
+  } catch (error) {
+    res.status(409).send({ message: error.message });
+  }
+};
 
 const createCouriers = async (req, res) => {
-    const courier = req.body;
-  
-    const newCourier = new courierMessage(courier);
-    try {
-      await newCourier.save();
-      res.status(200).send(newCourier);
-    } catch (error) {
-      res.status(409).json({ message: error.message });
-    }
+  const courier = req.body;
+
+  const newCourier = new courierMessage(courier);
+  try {
+    await newCourier.save();
+    res.status(200).send(newCourier);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
 };
-  
-exports.deleteCourier = async(req, res) =>{
-    // delete requested ID
-    const {_id} = req.body;
 
-    const courier = await courierMessage.findByIdAndDelete(_id);
-  
-    // console.log(courier);
-    try{
-        res.status(201).json({message: "Successful"});
-    }catch(err){
-      // console.log("not deleted");
-        res.status(409).json({message: err.message});
-    }
-    
-}
+exports.deleteCourier = async (req, res) => {
+  // delete requested ID
+  const { _id } = req.body;
 
+  const courier = await courierMessage.findByIdAndDelete(_id);
 
-  module.exports = { getCouriers, createCouriers };
+  // console.log(courier);
+  try {
+    res.status(201).json({ message: "Successful" });
+  } catch (err) {
+    // console.log("not deleted");
+    res.status(409).json({ message: err.message });
+  }
+};
+
+module.exports = { getCouriers, createCouriers };
