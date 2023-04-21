2ba2efc4bdf8cc1cac5cf39000f1d74fba3db567
Prakarsh-Mathur02 <202001012@daiict.ac.in>
2023-04-22 00:55:14 +0530

    Homepage update

--- client/src/components/homepage/mainpage.js
+++ client/src/components/homepage/mainpage.js
@@ -1,14 +1,26 @@
 import React from "react";
 import Sidebar from "./sidebar";
 import Navbar from "./navbar";
+import Notices from "./Notices";
+import CourierBoard from "./Courier_board";
+import Carousels from "./Carousel";
+import "./mainpage_style.css"
 
-function mainpage(){
+function Mainpage(){
     return(
-        <div>
-            <Navbar/>
-            <Sidebar/>
+        <div className="container">
+            {/* <Sidebar/> */}
+            <div>
+                <Notices/>
+                <div style={{marginTop: '10px'}}>
+                    <CourierBoard/>
+                </div>
+            </div>
+            <div>
+                <Carousels/>
+            </div>
         </div>
     );
 };
 
-export default mainpage;
+export default Mainpage;
--- client/src/components/homepage/navbar.js
+++ client/src/components/homepage/navbar.js
@@ -1,14 +1,29 @@
 import React from "react";
+import {GiHamburgerMenu} from "react-icons/gi"
 import "./navbar_style.css";
 
 const Navbar = () => {
   return (
-    <nav className="topnav">
-        <a className="active" href="/">Home</a>
-        <li>
-          <a className="alignright" href="/login">Login</a>
-        </li>
-      </nav>
+    // <nav className="topnav">
+    //     <a className="active" href="/">Home</a>
+    //     <li>
+    //       <a className="alignright" href="/login">Login</a>
+    //     </li>
+    //   </nav>
+    <nav className="navbar">
+    <div className="dropdown">
+      <button className="dropbtn"><GiHamburgerMenu /></button>
+      <div className="dropdown-content">
+        <a href="/NoticeBoard">Notice Board</a>
+        <a href="complaints">Complaints</a>
+        <a href="/couriers">Couriers</a>
+        <a href="/lostnfound">Lost and found</a>
+
+      </div>
+    </div>
+    <a href="/" className="home-btn">Home</a>
+    <a href="/login" className="login-btn">Login</a>
+  </nav>
   );
 };
 
