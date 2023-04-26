5b5b26d4ec4e234ff5fd2cb82f93255f7e08be87
Harsh Patel <hcp4902@gmail.com>
2023-04-27 04:29:52 +0530

    Update CSS of notice

--- client/src/components/Notices/NoticeBoard.js
+++ client/src/components/Notices/NoticeBoard.js
@@ -1,21 +1,30 @@
 import React, { useState } from "react";
 import "./add_notice.css";
+import "./NoticeBoard.css"
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Button from "@mui/material/Button";
+import Slide from "@mui/material/Slide";
+import { TextField } from "@mui/material";
 
 function NoticeBoard() {
   const [examples, setExamples] = useState([
     {
       id: 1,
       heading: "H1",
-      content: "Notice 1",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sagittis pharetra libero ac dapibus. Duis pulvinar, tortor et feugiat scelerisque, ligula metus tincidunt ipsum, ut euismod dui sem ut lacus. Vestibulum malesuada, nunc id finibus dapibus, nisl magna commodo ipsum, sed mattis turpis metus ut ex. Suspendisse potenti. Vivamus condimentum cursus leo, et euismod neque congue laoreet. Ut finibus ex non ex maximus, et ornare nisi volutpat. Quisque cursus purus ac ultrices fermentum.",
       writer: "Admin 1",
       createdAt: new Date(),
     },
     {
       id: 2,
       heading: "H2",
-      content: "Notice 2",
+      content: "Morbi blandit nulla sit amet dictum rhoncus. In maximus mauris porta velit pellentesque mollis. Nam convallis, lectus at cursus consequat, turpis enim bibendum augue, quis tincidunt sem augue id sapien. Phasellus luctus eu odio vel dictum. Morbi porttitor congue lacus sed scelerisque. Vivamus feugiat id orci sit amet mollis. Suspendisse volutpat vehicula porta. Morbi nec ullamcorper augue. Donec iaculis congue nisl, non pharetra sem. Donec ac posuere sem, at laoreet dui.",
       writer: "Admin 2",
       createdAt: new Date(),
     },
@@ -36,6 +45,24 @@ function NoticeBoard() {
   const [filter, setFilter] = useState("all");
   const [selectedDate, setSelectedDate] = useState(null);
   const [selectedAuthor, setSelectedAuthor] = useState("");
+
+
+  const handleHeadingChange = (e) => {
+    setHeading(e.target.value);
+  };
+
+  const handlecontentChange = (e) => {
+    setcontent(e.target.value);
+  };
+
+  const handlewriterChange = (e) => {
+    setwriter(e.target.value);
+    console.log("writer");
+  };
+
+  const [Heading, setHeading] = useState("");
+  const [content, setcontent] = useState("");
+  const [writer, setwriter] = useState("");
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -155,6 +182,17 @@ function NoticeBoard() {
     borderRadius: "5px",
   };
 
+  console.log(filteredExamples)
+
+  const [open, setOpen] = React.useState(false);
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <div className="App">
       <html>
@@ -176,13 +214,15 @@ function NoticeBoard() {
 
             <div className="FilterBlock">
               <label htmlFor="filter">Filter by:</label>
+              <div className="blocks">
               <select id="filter" value={filter} onChange={handleFilterChange}>
                 <option value="all">All Notices</option>
                 <option value="date">Date</option>
                 <option value="author">Author</option>
               </select>
+              </div>
               {filter === "date" && (
-              <div>
+              <div className="blocks">
                 <label htmlFor="datePicker">Select Date:</label>
                 <DatePicker
                   id="datePicker"
@@ -192,7 +232,7 @@ function NoticeBoard() {
               </div>
               )}
               {filter === "author" && (
-                <div>
+                <div className="blocks">
                   <label htmlFor="author">Select Author:</label>
                   <select
                     id="author"
@@ -208,64 +248,75 @@ function NoticeBoard() {
                   </select>
                 </div>
             )}
-
             </div>
             
+            <br></br>
+
             <div className="accordion" id="accordionExample">
-              {filteredExamples.map((example, index) => (
-                <div key={index} className="accordion-item">
-                  <h2 className="accordion-header">
-                    <button
-                      className={`accordion-button ${
-                        openIndex === index ? "" : "collapsed"
-                      }`}
-                      type="button"
-                      onClick={() => handleAccordionClick(index)}
-                      aria-expanded={openIndex === index ? "true" : "false"}
-                      aria-controls={`collapse-${index}`}
-                    >
-                      {example.heading}, Created by: {example.writer},{" "}
-                      {example.createdAt.toLocaleString()}
-                    </button>
-                  </h2>
-                  <div
-                    id={`collapse-${index}`}
-                    className={`accordion-collapse collapse ${
-                      openIndex === index ? "show" : ""
-                    }`}
-                    data-bs-parent="#accordionExample"
-                  >
-                    <div className="accordion-body">
-                      <strong>{example.content}</strong>
-                      <div style={{ float: "right" }}>
-                        <button
-                          onClick={() => handleEdit(example.id)}
-                          style={{
-                            backgroundColor: "blue",
-                            color: "white",
-                            fontSize: "100%",
-                            marginRight: "5px",
-                          }}
-                        >
-                          Edit
-                        </button>
-                        <button
-                          onClick={() => handleDelete(example.id)}
-                          style={{
-                            backgroundColor: "red",
-                            color: "white",
-                            fontSize: "100%",
-                          }}
-                        >
-                          Delete
-                        </button>
-                      </div>
-                    </div>
-                  </div>
+              {filteredExamples.map((example, index) =>{ return(
+                // <div key={index} className="accordion-item">
+                //   <h2 className="accordion-header">
+                //     <button
+                //       className={`accordion-button ${
+                //         openIndex === index ? "" : "collapsed"
+                //       }`}
+                //       type="button"
+                //       onClick={() => handleAccordionClick(index)}
+                //       aria-expanded={openIndex === index ? "true" : "false"}
+                //       aria-controls={`collapse-${index}`}
+                //     >
+                //       {example.heading}, Created by: {example.writer},{" "}
+                //       {example.createdAt.toLocaleString()}
+                //     </button>
+                //   </h2>
+                //   <div
+                //     id={`collapse-${index}`}
+                //     className={`accordion-collapse collapse ${
+                //       openIndex === index ? "show" : ""
+                //     }`}
+                //     data-bs-parent="#accordionExample"
+                //   >
+                //     <div className="accordion-body">
+                //       <strong>{example.content}</strong>
+                //       <div style={{ float: "right" }}>
+                //         <button
+                //           onClick={() => handleEdit(example.id)}
+                //           style={{
+                //             backgroundColor: "blue",
+                //             color: "white",
+                //             fontSize: "100%",
+                //             marginRight: "5px",
+                //           }}
+                //         >
+                //           Edit
+                //         </button>
+                //         <button
+                //           onClick={() => handleDelete(example.id)}
+                //           style={{
+                //             backgroundColor: "red",
+                //             color: "white",
+                //             fontSize: "100%",
+                //           }}
+                //         >
+                //           Delete
+                //         </button>
+                //       </div>
+                //     </div>
+                //   </div>
+                // </div>
+                <>
+                <div key={index} className="noticeBlock">
+                  <h3>{example.heading}</h3>
+                  <p>{example.content}</p> 
+                  <h7>
+                  {example.writer}
+                  </h7>
                 </div>
-              ))}
+                </>
+              );
+              })}
             </div>
-            <form onSubmit={handleFormSubmit} style={addNoticeFormStyles}>
+            {/* <form onSubmit={handleFormSubmit} style={addNoticeFormStyles}>
               <h3>{editing ? "Edit Notice" : "Add New Notice"}</h3>
               <div className="form-group">
                 <label htmlFor="heading">Heading:</label>
@@ -302,7 +353,56 @@ function NoticeBoard() {
               <button type="submit" className="btn-add-notice">
                 {editing ? "Update Notice" : "Add Notice"}
               </button>
-            </form>
+            </form> */}
+            
+            <button type="submit" className="addNew" onClick={handleClickOpen}>Add New Complain</button>
+            
+            <Dialog
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="form-dialog-title"
+            >
+              <DialogTitle id="form-dialog-title">Add Notice</DialogTitle>
+              <DialogContent>
+                <DialogContentText>
+                  To add a notice, please enter the heading, content and writer name.
+                </DialogContentText>
+                <TextField
+                  autoFocus
+                  margin="dense"
+                  id="Heading"
+                  label="Heading"
+                  type="text"
+                  fullWidth
+                  onChange={handleHeadingChange}
+                />
+                <TextField
+                  margin="dense"
+                  id="content"
+                  label="content"
+                  type="text"
+                  fullWidth
+                  onChange={handlecontentChange}
+                />
+                <TextField
+                  margin="dense"
+                  id="writer"
+                  label="writer"
+                  type="text"
+                  fullWidth
+                  onChange={handlewriterChange}
+                />
+              </DialogContent>
+
+              <DialogActions>
+                <Button onClick={handleClose} color="primary">
+                  Cancel
+                </Button>
+                <Button onClick={handleFormSubmit} color="primary">
+                  Add
+                </Button>
+              </DialogActions>
+            </Dialog>
           </div>
         </body>
       </html>
