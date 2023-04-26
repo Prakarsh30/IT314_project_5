36d6490e6e77ec0133e2dbdd335c9953058e0537
Kamal Patel <kamal1402patel@gmail.com>
2023-04-27 03:15:13 +0530

    rewrite path for netlify

--- server/app.js
+++ server/app.js
@@ -45,7 +45,7 @@ const loginRoute = require("./routes/login");
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "*",
   })
 );
 app.use(express.urlencoded({ extended: true }));
